2fb52f9215d7acba76a02a7024a2098487298317
Cameron <cameron.calhoun01@gmail.com>
2025-05-10 20:51:29 -0400

    scrollable +  nav bar started

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,40 @@
 "use client";
 import { motion } from "framer-motion";
 import Image from 'next/image';
+import ScrollIndicator from "@/components/ScrollIndicator";
+import { useEffect, useState, useRef } from "react";
+
+
 
 export default function Home() {
-    return (
-        <div className="text-center text-camwhite min-h-screen p-2 pb-20 gap-16 sm:p-20 ">
+    const [isSticky, setIsSticky] = useState(false)
+    const stickyRef = useRef<HTMLDivElement>(null)
 
-            <div className="flex flex-col items-center gap-15 w-auto ">
+    useEffect(() => {
+        const handleScroll = () => {
+            if (stickyRef.current) {
+                const rect = stickyRef.current.getBoundingClientRect()
+
+                if (rect.top <= 0) {
+                    setIsSticky(true)
+                } else {
+                    setIsSticky(false)
+                }
+            }
+
+        }
+
+        window.addEventListener("scroll", handleScroll)
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll)
+        }
+    }, [])
+
+    return (
+        <div className="text-center text-camwhite min-h-screen p-2 pb-20 gap-12 sm:p-20 relative">
+
+            <div className="flex flex-col items-center gap-4 w-auto  ">
                 <div className="flex flex-col items-center gap-3 w-auto ">
                     {/* Welcome message */}
                     <div className="Welcome text-3xl md:text-6xl pointer-events-auto">
@@ -27,34 +55,39 @@ export default function Home() {
                         </motion.h1>
                     </div>
 
-                    {/* Logo + divider */}
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 1.3, delay: 1.5 }}
-                        className="flex flex-col gap-2  drop-shadow-textoutline pointer-events-auto "
-                    >
-                        <Image
-                            src='/camLogo.png'
-                            alt='Cameron Calhoun'
-                            width='400'
-                            height='50'
-                            className="md:w-2xl"
-                        />
-                        <div className="border-camgreen border rounded-xl"></div>
-                    </motion.div>
-
                 </div>
+
+                {/* Logo + divider */}
+                <motion.div
+                    ref={stickyRef}
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1.3, delay: 1.5 }}
+                    className={`mb-10 sticky top-0 z-50 flex flex-col gap-2
+                               drop-shadow-textoutline pointer-events-auto  transition-all ease-in-out duration-500
+                                ${isSticky ? 'backdrop-blur-md w-screen' : ''}`}
+                >
+                    <Image
+                        src='/camLogo.png'
+                        alt='Cameron Calhoun'
+                        width='400'
+                        height='50'
+                        className={`md:w-2xl ${isSticky ? 'mx-auto ' : ''}`}
+                    />
+                    <div className="border-camgreen border rounded-xl"></div>
+                </motion.div>
+
                 {/* Content */}
                 <motion.div
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
                     transition={{ duration: 1.3, delay: 1.8 }}
-                    className="flex justify-center items-center"
+                    className="flex flex-col gap-50 justify-center items-center"
                 >
+                    {/* Intro Block */}
                     <div className="flex flex-col md:flex-row items-center 
                                     w-full md:w-5/6 gap-20 border-camgreen border-3 
-                                    p-4 rounded-xl bg-camblack/[30%] backdrop-blur-xs" >
+                                    p-4 rounded-xl bg-camblack/[30%] backdrop-blur-xs relative" >
                         <div className="md:flex-1 lg:flex-[2] ">
                             <p className="text-base md:text-lg lg:text-2xl pointer-events-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                         </div>
@@ -69,6 +102,44 @@ export default function Home() {
                         </div>
                     </div>
 
+                    <ScrollIndicator />
+
+                    {/* Secondary Block */}
+                    <div className="flex flex-col md:flex-row items-center 
+                                    w-full md:w-5/6 gap-20 border-camgreen border-3 
+                                    p-4 rounded-xl bg-camblack/[30%] backdrop-blur-xs relative"
+                        id="second-block">
+                        <div className="md:flex-1 lg:flex-[2] ">
+                            <p className="text-base md:text-lg lg:text-2xl pointer-events-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div className="md:flex-1 lg:flex-[1] flex items-center justify-center pointer-events-auto">
+                            <Image
+                                src='/cat.jpg'
+                                alt='cool cat that shall serve as a placeholder'
+                                width={350}
+                                height={400}
+                                className="rounded-xl object-cover max-w-full h-auto"
+                            />
+                        </div>
+                    </div>
+
+                    {/* Third Block */}
+                    <div className="flex flex-col md:flex-row items-center 
+                                    w-full md:w-5/6 gap-20 border-camgreen border-3 
+                                    p-4 rounded-xl bg-camblack/[30%] backdrop-blur-xs relative" >
+                        <div className="md:flex-1 lg:flex-[2] ">
+                            <p className="text-base md:text-lg lg:text-2xl pointer-events-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                        </div>
+                        <div className="md:flex-1 lg:flex-[1] flex items-center justify-center pointer-events-auto">
+                            <Image
+                                src='/cat.jpg'
+                                alt='cool cat that shall serve as a placeholder'
+                                width={350}
+                                height={400}
+                                className="rounded-xl object-cover max-w-full h-auto"
+                            />
+                        </div>
+                    </div>
                 </motion.div>
             </div>
         </div>
---
+++ src/components/ScrollIndicator.tsx
@@ -0,0 +1,44 @@
+"use client";
+import { useEffect, useState } from "react";
+
+const ScrollIndicator = () => {
+    const [visible, setVisible] = useState(true);
+
+    useEffect(() => {
+        const target = document.getElementById("second-block")
+
+        if (!target) {
+            console.warn("No element with id 'second-block' found.")
+            return
+        }
+
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                setVisible(!entry.isIntersecting);
+            },
+            {
+                threshold: 0.1,
+            }
+        )
+
+        observer.observe(target);
+        return () => observer.disconnect();
+    }, [])
+
+    return (
+        <a
+            href="#demo"
+            className={`fixed bottom-6 left-1/2 transform -translate-x-1/2 z-50 transition-opacity duration-300 ${visible ? "opacity-100" : "opacity-0 pointer-events-none"
+                }`}
+        >
+            <div className="flex flex-col items-center justify-center space-y-1">
+                <span className="w-5 h-5 border-r-2 border-b-2 border-white transform rotate-45 animate-scroll"></span>
+                <span className="w-5 h-5 border-r-2 border-b-2 border-white transform rotate-45 animate-scroll delay-200"></span>
+                <span className="w-5 h-5 border-r-2 border-b-2 border-white transform rotate-45 animate-scroll delay-400"></span>
+            </div>
+        </a>
+    )
+}
+
+export default ScrollIndicator
+
